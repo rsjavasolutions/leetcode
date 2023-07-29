061fc6b2fff318cfd254f03d0f1389e872fc2c9c
rsjavasolutions <stan.robert.1986@gmail.com>
2023-07-29 09:24:48 +0200

    Add add two numbers solution

---
+++ src/main/java/org/example/addtwonumbers/ListNode.java
@@ -0,0 +1,23 @@
+package org.example.addtwonumbers;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class ListNode {
+
+    public int val;
+
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/test/groovy/org/example/addtwonumbers/SolutionSpec.groovy
@@ -0,0 +1,22 @@
+package org.example.addtwonumbers
+
+import spock.lang.Specification
+
+class SolutionSpec extends Specification {
+
+    def solution = new Solution();
+
+    def "should add the two numbers and return the sum as a linked list."() {
+
+        expect:
+        solution.addTwoNumbers(node1, node2) == result
+
+        where:
+        node1                                             | node2                                             || result
+        new ListNode(2, new ListNode(4, new ListNode(3))) | new ListNode(5, new ListNode(6, new ListNode(4))) || new ListNode(7, new ListNode(0, new ListNode(8)))
+        new ListNode(0)                                   | new ListNode(0)                                   || new ListNode(0)
+        new ListNode(1, new ListNode(1, new ListNode(1))) | new ListNode(1, new ListNode(1, new ListNode(1))) || new ListNode(2, new ListNode(2, new ListNode(2)))
+
+    }
+}
+
